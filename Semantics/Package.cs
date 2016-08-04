da913e12e582bc518b06daceed83c60fcfd76e49
Jeff Walker Code Ranger <Jeff@WalkerCodeRanger.com>
2016-08-04 12:02:22 -0400

    add Semantics project

---
+++ Semantics/Package.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semantics
+{
+	/// <summary>
+	/// A compiled package
+	/// </summary>
+    public class Package
+    {
+    }
+}
--- Semantics/Properties/AssemblyInfo.cs
+++ Semantics/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PreAdamant.Compiler.Analyzer")]
+[assembly: AssemblyTitle("PreAdamant.Compiler.Semantics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3514772b-ee43-434b-8ca4-8186e060ddbf")]
+[assembly: Guid("9356799e-68cb-4aa1-ac5a-8cbc90fe6355")]
 
 // Version information for an assembly consists of the following four values:
 //
